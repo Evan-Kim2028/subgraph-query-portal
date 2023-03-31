f372d7efb066d113ebbbe5d80f5d40140a3fcf2d
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-03-31 18:10:01 -0400

    update test filter

--- tests/test_dex_swaps_and_tokens.py
+++ tests/test_dex_swaps_and_tokens.py
@@ -9,22 +9,22 @@ endpoint = 'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-arbitrum'
 # instantiate dex class. Dex stores dex-related query functions
 my_dex = Dex(endpoint)
 
-# convert start_time and end_time to unix timestamps
-start_timestamp = int(datetime(2023, 3, 9).timestamp())
-end_timestamp = int(datetime(2023, 3, 13).timestamp())
+# # convert start_time and end_time to unix timestamps
+# start_timestamp = int(datetime(2023, 3, 9).timestamp())
+# end_timestamp = int(datetime(2023, 3, 13).timestamp())
 
-# define a filter dictionary to customize the query search
-filter_dict = {
-    'timestamp_gte': start_timestamp,
-    'timestamp_lte': end_timestamp,
-}
+# # define a filter dictionary to customize the query search
+# filter_dict = {
+#     'timestamp_gte': start_timestamp,
+#     'timestamp_lte': end_timestamp,
+# }
 
 # specify query size
 query_size = 125
 
 df = my_dex.query_swaps(
     query_size=query_size,
-    filter_dict=filter_dict,
+    # filter_dict=filter_dict,
     save_data=True,
     add_endpoint_col=True
     )
