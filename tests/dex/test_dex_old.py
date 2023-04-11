69ef8303443cdc8721673b4d678e7d25f5cc28cf
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-04-11 15:04:42 -0400

    rename unused tests to _old

--- tests/dex/test_dex_old.py
+++ tests/dex/test_dex_old.py
@@ -15,14 +15,15 @@ my_dex = Dex(endpoint)
 # Test firehose (complex query)
 #
 #####################################################
-my_dex.query_firehose(query_paths = ['pool_name', 'tokenIn_name', 'tokenOut_name'])
+my_dex.query_firehose()
+# my_dex.query_firehose(query_paths = 'pool_id')
 
 
 #####################################################
 # Test functionality of individual queries
 #
 #####################################################
-# my_dex.query_swaps(query_paths = ['id', 'timestamp'])
+# my_dex.query_swaps()
 # my_dex.query_pools()
 # my_dex.query_tokens(query_paths = ['id', 'symbol'])
 
@@ -33,7 +34,9 @@ my_dex.query_firehose(query_paths = ['pool_name', 'tokenIn_name', 'tokenOut_name
 #####################################################
 # filter_dict = {
 #     'timestamp_gte': int((datetime.today() - timedelta(days=1)).timestamp()),
-#     'timestamp_lte': int(datetime.today().timestamp())
+#     'timestamp_lte': int(datetime.today().timestamp()),
+#     'token_symbol_in': ['USDC', 'WETH'],
+#     'pool_id_in': ['0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8']
 # }
 # my_dex.query_swaps(filter_dict = filter_dict)
 
