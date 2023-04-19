d6536bbc3fe6d33d53c4fac301c82dbdcad004e4
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-04-19 09:39:19 -0400

    fix bug with query_swap empty filter dict

--- src/queryportal/helpers.py
+++ src/queryportal/helpers.py
@@ -126,32 +126,37 @@ def create_query_path(default_query_path: FieldPath, query_paths: list[str]) -> 
 
 
 def create_filter_dict(filter_dict: dict) -> dict:
-    keyword_list = ['in', 'not', 'gt', 'gte', 'lt', 'lte', 'not_in', 'contains', 'not_contains']
+    # if filter_dict is empty, return empty dict. Otherwise, continue
+    match filter_dict:
+        case {}:
+            return {}
+        case _:
+            keyword_list = ['in', 'not', 'gt', 'gte', 'lt', 'lte', 'not_in', 'contains', 'not_contains']
 
-    output_dict = {}
+            output_dict = {}
 
-    for key in filter_dict.keys():
-        # check if last _ is followed by keyword. Split into a list
-        key_parts = key.split('_')
-        if key_parts[-1] in keyword_list:   # check if key ends with a keyword
-            # combine key_parts[-1] and key_parts[-2]
-            new_key = '_'.join(key_parts[-2:])
-            # drop the last two elements from key_parts
-            key_parts = key_parts[:-2]
-            # append new_key
-            key_parts.append(new_key)
+            for key in filter_dict.keys():
+                # check if last _ is followed by keyword. Split into a list
+                key_parts = key.split('_')
+                if key_parts[-1] in keyword_list:   # check if key ends with a keyword
+                    # combine key_parts[-1] and key_parts[-2]
+                    new_key = '_'.join(key_parts[-2:])
+                    # drop the last two elements from key_parts
+                    key_parts = key_parts[:-2]
+                    # append new_key
+                    key_parts.append(new_key)
 
-    # make a new dictionary based off of the key_parts
-    temp_dict = output_dict
-    for i in range(len(key_parts)):
-        if key_parts[i] not in temp_dict:
-            if i == len(key_parts) - 1:
-                temp_dict[key_parts[i]] = {}
-                temp_dict[key_parts[i]] = filter_dict[key]
-            else:
-                new_key = key_parts[i] + '_'
-                temp_dict[new_key] = {}
-                temp_dict = temp_dict[new_key]
+            # make a new dictionary based off of the key_parts
+            temp_dict = output_dict
+            for i in range(len(key_parts)):
+                if key_parts[i] not in temp_dict:
+                    if i == len(key_parts) - 1:
+                        temp_dict[key_parts[i]] = {}
+                        temp_dict[key_parts[i]] = filter_dict[key]
+                    else:
+                        new_key = key_parts[i] + '_'
+                        temp_dict[new_key] = {}
+                        temp_dict = temp_dict[new_key]
 
-    print(output_dict)
-    return output_dict
+            print(output_dict)
+            return output_dict
