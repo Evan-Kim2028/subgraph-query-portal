d83fd8d3353ded1139fbe98e078b3b46546a7bbb
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-04-19 13:01:02 -0400

    remove old query abstract method

--- src/queryportal/queryinterface.py
+++ src/queryportal/queryinterface.py
@@ -39,37 +39,6 @@ class QueryInterface(ABC):
         # Return the converted dataframe
         return final_df
     
-
-# OLD query() method! Saving just incase...
-    # @abstractmethod
-    # @timeit  # Decorator that times the execution of the query method
-    # @df_describe  # Decorator that adds a description of the returned dataframe to the console
-    # def query(
-    #         self, 
-    #         query_path: FieldPath | list[FieldPath],
-    #         saved_file_name: str = None,
-    #         add_endpoint_col: bool = True
-    #         ) -> pl.DataFrame:
-    #     """
-    #     Abstract query method. Requires the following:
-    #     - query path: A string representing the query to be executed
-
-    #     The query method is responsible for obtaining query results from a data source, converting the results to a pandas dataframe, and returning the dataframe.
-    #     """
-
-    #     # Obtain pandas dataframe of query results
-    #     df = self.sg.query_df(query_path)
-
-    #     # Convert dataframe to Polars dataframe
-    #     converted_df = to_polars(df)
-
-    #     # If a file name is provided, save the dataframe as a CSV file
-    #     if saved_file_name is not None:
-    #         save_file(converted_df, saved_file_name)
-
-    #     # Return the converted dataframe
-    #     return converted_df
-
     
     def add_synthetic_fields(self):
         """
