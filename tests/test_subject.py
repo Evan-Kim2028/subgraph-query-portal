9e2a7dcdd0cea044f3aecf26b4c370f8c15d4766
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-04-21 14:14:49 -0400

    update test to be multichain

--- tests/test_subject.py
+++ tests/test_subject.py
@@ -4,7 +4,9 @@ from queryportal.new_dex import Dex
 
 dex_endpoints = [
     'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum',
-    'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-arbitrum'
+    'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-arbitrum',
+    'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-polygon',
+    'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-optimism',
 ]
 
 # loading the Subject with dex_endpoints
@@ -20,35 +22,25 @@ filter_dict = {
 }
 
 query_paths = [
-    # 'timestamp', 
-    # 'hash',
+    'hash',
     'pool_name', 
-    # 'pool_inputTokens_name', # this returns an array of values which doubles amount of rows due to flattening logic in Subgrounds
+    # 'pool_inputTokens_name', #This is a nested struct. Although there is logic to handle a single struct, it is not yet implemented for nested structs. Not sure if it should be either.
     'tokenIn_symbol', 
     'tokenOut_symbol', 
-    # 'amountOutUSD', 
-    # 'amountInUSD',
-    # 'amountOut',
-    # 'amountIn',
+    'amountOutUSD', 
+    'amountInUSD',
+    'amountOut',
+    'amountIn',
     ]
 
-
-# run a query like normal with 'uniswap-v3-ethereum' key
-univ3_eth = dex.query_swaps(
-    subgraph_name='uniswap-v3-ethereum', 
-    query_paths=query_paths,
-    filter_dict = filter_dict
-    )
-univ3_arb = dex.query_swaps(
-    subgraph_name='uniswap-v3-arbitrum',
+for subgraph_name in list(dex.subject.subgraphs.keys()):
+    df = dex.query_swaps(
+    subgraph_name=subgraph_name, 
     query_paths=query_paths,
     filter_dict = filter_dict
     )
 
-
-print(univ3_eth.head(5))
-print(univ3_arb.head(5))
-
+    print(df.head(5))
 
 
 
