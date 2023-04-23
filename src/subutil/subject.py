2c8a4992f0bdaa1ef50464d1d2f6c2c25371be8f
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-04-23 13:39:31 -0400

    add queryable schema lookup

--- src/subutil/subject.py
+++ src/subutil/subject.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from subutil.helpers import *
 
 from subgrounds import Subgrounds
-from subgrounds.schema import TypeRef
+from subgrounds.schema import TypeRef, SchemaMeta
 from subgrounds.subgraph import Subgraph
 from subgrounds.subgraph import SyntheticField
 
@@ -33,17 +33,33 @@ class Subject:
                 self.subgraphs[endpoints.split('/')[-1]] = self.sg.load(endpoints)
 
 
+    def getQueryFields(self, endpoint: str, schema: str) -> list[str]:
+        """
+        Get all queryable fields from the subgraph schema.
+        :return: list[str] of queryable fields from the subgraph schema
+        """
+        query_field_paths = self.getSchemaFields(endpoint, schema)
+
+        return query_field_paths
+
+
+    def getSchemaFields(self, endpoint: str, schema_str: str) -> list[str]:
+        """
+        getSubgraphField gets a fields list from a subgraph schema.
+        :param str schema_str: Schema object name to get fields list from
+        :param str operation: Enter one of the following - 'Query', 'Mutation', or 'Subscription'. Default is 'Query' because that is most commonly used.
+        :return: strings field list from a Subgraph schema
+        """
+        return list(field.name for field in self.subgraphs[endpoint].__getattribute__(schema_str)._object.fields)
+    
+
     def getSubgraphSchema(self, sg: Subgraph) -> list[str]:
         """
         getSubgraphSchema gets the Subgraph schema and returns a list.
         """
         return list(name for name, type_ in sg._schema.type_map.items() if type_.is_object)
     
-    # def get_entity_cols(self, sg: Subgraph) -> list[str]:
-    #     """
-    #     get_entity_cols gets the columns for a specific entity.
-    #     """
-    #     return list((field.name, TypeRef.graphql(field.type_)) for field in sg._object.fields)
+
 
 
     
