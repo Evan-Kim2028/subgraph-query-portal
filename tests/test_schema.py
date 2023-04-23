612f84865519944ed459e4e990c65150240f12b1
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-04-23 13:39:39 -0400

    update schema test

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,19 +1,28 @@
 from subutil.subject import Subject
 
-sub = Subject('https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum')
+sub = Subject([
+    'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum',
+    'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-arbitrum'
+    ])
 
 
-print(list(sub.subgraphs.keys())[0])
-
+# get first subgraph endpoint key
 sg_key = list(sub.subgraphs.keys())[0]
 
+# load subgraph object for the specific endpoint key
 sg_obj = sub.subgraphs[sg_key]
 
+# get all schema entities
+schema_list = sub.getSubgraphSchema(sg_obj)
+print(f'\nschema_list: \n{schema_list}')
 
-my_list = sub.getSubgraphSchema(sg_obj)
-print(my_list)
+# select queryable schema entities
+schema_fp_list = schema_list.index('Query')
 
+query_field = sub.getQueryFields('uniswap-v3-ethereum', schema_list[schema_fp_list])
+print(f'\nquery_field: \n{query_field}')
 
-# my_list = sub.get_entity_cols(my_list[0])
+print(f'\nthe first query field is: {query_field[0]}')
+# schema_list = sub.get_entity_cols(schema_list[0])
 
-print('done')
+print('\ndone')
