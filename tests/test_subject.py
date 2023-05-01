489ae5e41baf2d478f8d6f1b8f8266cf20e1ae12
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-05-01 18:56:51 -0400

    test decentralized endpoint loading

--- tests/test_subject.py
+++ tests/test_subject.py
@@ -1,15 +1,26 @@
 from subutil.subject import Subject
 
-endpoint = 'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum'
-# load Subject
+endpoint = 'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum'    # hosted endpoint
+    # decentralized endpoint
+
+
+# load Subject with hosted service endpoints
 sub = Subject(endpoint)
+print(f'subject endpoints: {sub.subgraphs.keys()}')
+
+# load decentralized endpoint
+sub.load_decentralized_endpoints(
+    {'univ3_decentralized': 'https://api.playgrounds.network/v1/proxy/subgraphs/id/FQ6JYszEKApsBpAmiHesRsd9Ygc6mzmpNRANeVQFYoVX'
+     }
+     )
+
+print(f'subject endpoints: {sub.subgraphs.keys()}')
 
 
-print(f'subject endpoints: {sub.endpoints}')
 
 # test load schema logic
-query_dict = sub.load_schema(sub.subgraphs['uniswap-v3-ethereum'])
-# print(type(query_dict)) # dict
-print(f'keys are {query_dict.keys()}')
+query_dict = sub.load_schema(sub.subgraphs['univ3_decentralized'])
+print(query_dict.keys())
 
+# print(type(query_dict)) # dict
 print(f' one of the keys are swaps and it is type {type(query_dict["swaps"])}')
